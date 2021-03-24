f99a1f7928b1bdef52eeca79275b6797e77a187b
amine-sakka <43292310+amine-sakka@users.noreply.github.com>
2021-03-24 23:06:27 +0100

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello bitch");
-         System.out.println("die bitch");
-          System.out.println("f u");
+        
     }
 }
