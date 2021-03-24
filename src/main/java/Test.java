4311cadf88e785ff7e01dd96cfb8f18b8347084e
amine-sakka <aminesakka973@gmail.com>
2021-03-24 14:33:31 +0100

    die

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello bitch");
+         System.out.println("die bitch");
+          System.out.println("f u");
     }
 }
